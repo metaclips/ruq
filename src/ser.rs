c5031360fb1810bf370af108bc360944da682cf2
crustydev <ademola1145@gmail.com>
2022-10-10 18:32:04 +0100

    Implement serializer for --filter argument

---
+++ src/ser.rs
@@ -0,0 +1,450 @@
+use serde::{ser, de, Serialize};
+
+//use serde::de::{
+//    self, Deserialize, DeserializeSeed, EnumAccess, IntoDeserializer, 
+//    MapAccess, SeqAccess, VariantAccess, Visitor,
+//};
+//use std::ops::{AddAssign, MulAssign, Neg};
+use std::fmt::{self, Display};
+use regex::Regex;
+use serde_json::Value;
+
+use std::vec::Vec;
+use std::ops::RangeInclusive;
+
+use crate::parser::JsonParser;
+
+
+#[derive(Debug)]
+pub enum Error {
+    Message(String),
+    Eof,
+}
+
+impl ser::Error for Error {
+    fn custom<T: Display>(msg: T) -> Self {
+        Error::Message(msg.to_string())
+    }
+}
+
+impl de::Error for Error {
+    fn custom<T: Display>(msg: T) -> Self {
+        Error::Message(msg.to_string())
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::Message(msg) => write!(f, "{}", msg),
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct Serializer {
+    json: Value,
+    regex: Regex,
+    output: String,
+}
+
+pub fn parse_ser<T>(filter: &T, json_parser: &JsonParser) -> Result<String>
+where
+    T: Serialize,
+{
+    let mut serializer = Serializer {
+        json: json_parser.json.to_owned(),
+        regex: json_parser.filter_regex.to_owned(),
+        output: String::new()
+    };
+    filter.serialize(&mut serializer)?;
+    Ok(serializer.output)
+}
+
+impl Serializer {
+    fn get_filter_range(&self, query: String) -> Result<Vec::<RangeInclusive<usize>>> {
+        let chars = query.clone();
+        let chars = chars.chars();
+
+        let mut discard = false;
+        let mut found_filter = false;
+        let mut filter_index = 0;
+        let mut filter_range = vec![];
+
+        for (index, char) in chars.into_iter().enumerate() {
+            match char {
+                '"' | '\'' | '\\' => {
+                    discard = !discard;
+                }
+                '.' => {
+                    if discard || found_filter {
+                        continue;
+                    }
+
+                    found_filter = true;
+                    filter_index = index;
+                }
+                '}' | ']' | ',' | ' ' => {  
+                    if found_filter {
+                        let left_char = query.chars().nth(index - 1).unwrap();
+
+                        if left_char == '[' || left_char.is_numeric() {
+                            if query.chars().nth(index + 1).is_some() {
+                                continue;
+                            }
+
+                            filter_range.push(filter_index..=index);
+                        } else {
+                            filter_range.push(filter_index..=index - 1);
+                        }
+
+                        discard = false;
+                        found_filter = false;
+                    }
+                }
+                _ => {}
+            }
+
+            if query.chars().nth(index + 1).is_none() && found_filter {
+                filter_range.push(filter_index..=index);
+            }
+        }
+
+        Ok(filter_range)
+    }
+}
+
+impl<'a> ser::Serializer for &'a mut Serializer {
+    type Ok = ();
+
+    type Error = Error;
+
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+
+    fn serialize_bool(self, _v: bool) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_i8(self, _v: i8) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_i16(self, _v: i16) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_i32(self, _v: i32) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_i64(self, _v: i64) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_u8(self, _v: u8) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_u16(self, _v: u16) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_u32(self, _v: u32) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_u64(self, _v: u64) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_f32(self, _v: f32) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_f64(self, _v: f64) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_char(self, v: char) -> Result<()> {
+        self.serialize_str(&v.to_string())
+    }
+
+    fn serialize_str(self, v: &str) -> Result<()> {
+        let mut query = String::from(v);
+        let mut filter_range = self.get_filter_range(query.clone()).unwrap();
+
+        while let Some(range) = filter_range.pop() {
+            let start = *range.start();
+
+            let mut filter: String = query.drain(range).collect();
+            if self.regex.is_match(&filter) {
+                let mut value = self.json.clone();
+                for filter_capture in self.regex.captures_iter(&filter) {
+                    let key = filter_capture.name("key").unwrap().as_str();
+                    if !key.is_empty() {
+                        value = value.get(key).cloned().unwrap_or_default();
+                    }
+
+                    if let Some(e) = filter_capture.name("index") {
+                        value = value
+                            .get(e.as_str().parse::<usize>().unwrap())
+                            .cloned()
+                            .unwrap_or_default()
+                    }
+                }
+
+                filter = value.to_string()
+            } else {
+                panic!("Invalid filter {}", filter)
+            }
+
+            query.insert_str(start, filter.as_str());
+        }
+
+        self.output+=query.as_str();
+
+        Ok(())
+    }
+
+    fn serialize_bytes(self, _v: &[u8]) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_none(self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_some<T>(self, _value: &T) -> Result<()> 
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn serialize_unit(self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+    ) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_newtype_struct<T>(
+        self,
+        _name: &'static str,
+        _value: &T,
+     ) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn serialize_newtype_variant<T>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _value: &T
+    ) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq> {
+        unimplemented!()
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple> {
+        unimplemented!()
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize
+    ) -> Result<Self::SerializeTupleStruct> {
+        unimplemented!()
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize
+    ) -> Result<Self::SerializeTupleVariant> {
+        unimplemented!()
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap> {
+        unimplemented!()
+    }
+
+    fn serialize_struct(
+        self,
+        _name: &'static str,
+        _len: usize
+    ) -> Result<Self::SerializeStruct> {
+        unimplemented!()
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize
+    ) -> Result<Self::SerializeStructVariant> {
+        unimplemented!()
+    }
+
+}
+
+impl<'a> ser::SerializeSeq for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeTuple for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeTupleStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeTupleVariant for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeMap for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_key<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn serialize_value<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeStructVariant for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+
+
+
+
+
+
+
